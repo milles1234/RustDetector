9b8d5ce6efcf88310065e2f87536e6d88e88c2ee
unknown <dy020802003@gmail.com>
2026-01-23 16:37:40 +0530

    Landing Page done

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,82 +1,83 @@
-import React from 'react'
+import React from 'react';
+import company1 from "../assets/company1.jpg";
+import company2 from "../assets/company2.png";
+import company3 from "../assets/company3.jpg";
+import company4 from "../assets/company4.png";
+import { Brain, ClipboardMinus, FileUp, Play } from 'lucide-react';
 
 
 const Landing = () => {
-    return (
-         <div className="relative min-h-screen overflow-hidden bg-page-bg">
 
-     
-      <video
-        className="absolute inset-0 w-full h-full object-cover"
-        autoPlay
-        loop
-        muted
-        playsInline
-        src="/src/assets/landing-page.mp4"
-      />
+  
+  const logos = [company1, company2, company3, company4];
 
-      
-      <div className="absolute inset-0 bg-black/40" />
+  return (
+    <>
+   
+      <section className="relative min-h-screen overflow-hidden bg-page-bg">
+        <video
+          className="absolute inset-0 w-full h-full object-cover"
+          autoPlay
+          loop
+          muted
+          playsInline
+          src="/src/assets/landing-page.mp4"
+        />
 
-    
-      <div className="relative z-10 min-h-screen flex flex-col items-center justify-center text-center px-6">
-        <h1 className="text-white text-4xl md:text-6xl font-bold mb-6">
-          Detect Rust Before It Becomes a Problem
-        </h1>
+        <div className="absolute inset-0 bg-black/40" />
 
-        <h2 className="text-white text-lg md:text-2xl max-w-3xl">
-          AI-powered rust detection for industrial equipment. Get instant analysis,
-          severity reports, and maintenance recommendations in seconds.
-        </h2>
-      </div>
-            {/* <div>
-                Trusted by maintenance teams at:
-            </div>
-            <div>
-                <h2>Three Simple Steps to Safer Equipment</h2>
-                Upload Your Image
-                Drag and drop photos of metal surfaces, equipment, or infrastructure. Supports JPG, PNG, and batch uploads.
+        <div className="relative z-10 min-h-screen flex flex-col items-center justify-center text-center px-6">
+          <h1 className="text-white text-4xl md:text-6xl font-bold mb-15">
+            Detect Rust Before It Becomes a Problem
+          </h1>
 
+          <h2 className="text-white text-lg md:text-2xl max-w-3xl">
+            AI-powered rust detection for industrial equipment. Get instant analysis,
+            severity reports, and maintenance recommendations in seconds.
+          </h2>
 
-                and .......many more in this section
-            </div>
-            <div>
-                <h2>Everything You Need for Rust Management</h2>
-                <h3>Real-Time Analysis</h3>
-                Upload images and receive comprehensive rust analysis within seconds. No waiting, no complicated setup—just instant, accurate results powered by cutting-edge AI technology.
-
-
-
-                more section ......conirnuw
-            </div>
-            <div>befite section</div>
-            <h2>Why Choose Our Rust Detection AI?</h2>
-            **For Maintenance Teams:**
-
-- Reduce inspection time by 80%
-- Catch rust in early stages before costly damage occurs
-- Make data-driven maintenance decisions
-- Eliminate human error and inconsistency in visual inspections
-
-**For Operations Managers:**
-
-- Prevent unexpected equipment failures and downtime
-- Optimize maintenance budgets with predictive insights
-- Improve workplace safety by identifying structural weaknesses
-- Maintain compliance with safety regulations and documentation requirements
-
-**For Facility Owners:**
-
-- Extend equipment lifespan through proactive care
-- Reduce emergency repair costs by up to 60%
-- Protect asset value with regular monitoring
-- Access inspection data anywhere, anytime through cloud storage
-<div>Use Cases Section </div>
-<div>
-    social proof section
-</div> */}
+          <div className="flex gap-4 mt-6">
+            <button className="px-4 py-2 bg-alert-orange text-white rounded-2xl">
+              Start Free Analysis
+            </button>
+           
+            <button className="px-4 py-3 bg-alert-orange text-white rounded-2xl">
+              Watch Demo
+            </button>
+          </div>
         </div>
-    )
-} 
+      </section>
+     <section className='flex flex-col items-center px-3 min-h-screen overflow-hidden justify-center gap-23'>
+      <h1 className='text-6xl font-bold mb-10 text-industrial-navy text-center g'>Three Simple Steps to Safer Equipment</h1>
+     <div className='flex flex-cols gap-4 '>
+      <div className='flex  items-start gap-3'>
+        <FileUp className='shrink-0' size={36}/>
+       <div className='font-semibold'>Upload Your Image</div>
+       <span>Drag and drop photos of metal surfaces, equipment, or infrastructure. Supports JPG, PNG, and batch uploads.</span>
+       </div>
+       
+       <div className='flex items-start gap-4'>
+        <div className='flex  items-start gap-3'>
+          <Brain className='shrink-0' size={36}/>
+        <div className='font-semibold'>AI Analyzes </div>
+        <span>Drag and drop photos of metal surfaces, equipment, or infrastructure. Supports JPG, PNG, and batch uploads.</span>
+        </div>
+        
+       </div>
+       <div className='flex items-start gap-4'>
+        <div className='flex  items-start gap-3'>
+          <ClipboardMinus className='shrink-0' size={36}/>
+        <div className='font-semibold'>Get Actionable Insights</div>
+        <span>Download detailed reports with severity scores, visual heat maps, and maintenance recommendations to prevent equipment failure.</span>
+        </div>
+        </div>
+       
+       </div>
+     </section>
+     
+  
+    </>
+  );
+};
 
-export default Landing
+export default Landing;
